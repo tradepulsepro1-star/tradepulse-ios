0cd3afe5a89ea9a8cc0741ef13b8639340e8e6de
TradePulse <tradepluspro1@gmail.com>
2026-05-04 17:53:30 +0000

    build 45: replace UIGlassEffect with UIBlurEffect, fix ObjC selector conflict in WebViewViewportManager

--- ios/LeanIOS/WebViewViewportManager.swift
+++ ios/LeanIOS/WebViewViewportManager.swift
@@ -8,18 +8,23 @@ import WebKit
 
 @objc final class WebViewViewportManager: NSObject {
     @objc static let shared = WebViewViewportManager()
-    
+
     var currentUserScript: WKUserScript?
 
-    @objc func setViewportWithScale(_ scale: CGFloat, width: CGFloat, webView: WKWebView?) {
-        // no-op stub for compatibility
+    // Called from LEANUtilities.m: [WebViewViewportManager shared] setViewportWithScale:width:webView:
+    @objc(setViewportWithScale:width:webView:)
+    func setViewportWithScale(_ scale: CGFloat, width: CGFloat, webView: WKWebView?) {
+        // no-op
     }
-    
-    @objc func handleUrl(_ url: URL, query: [AnyHashable: Any], webView: WKWebView?, completion: @escaping ([AnyHashable : Any]) -> Void) {
+
+    @objc func handleUrl(_ url: URL, query: [AnyHashable: Any],
+                         webView: WKWebView?,
+                         completion: @escaping ([AnyHashable: Any]) -> Void) {
         completion([:])
     }
 
-    @objc func getViewportScale(webView: WKWebView?, completion: @escaping ([AnyHashable : Any]) -> Void) {
+    @objc func getViewportScale(webView: WKWebView?,
+                                completion: @escaping ([AnyHashable: Any]) -> Void) {
         completion(["zoom": 1])
     }
 
