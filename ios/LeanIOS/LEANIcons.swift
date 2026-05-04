87dd38f9b0d245cfc5b0eb1abcd7cbbcf65109f4
TradePulse <tradepluspro1@gmail.com>
2026-05-04 11:09:57 +0000

    build 19: remove MedianIcons import from LEANIcons - stub imageForIconIdentifier

--- ios/LeanIOS/LEANIcons.swift
+++ ios/LeanIOS/LEANIcons.swift
@@ -1,18 +1,12 @@
-//
-//  LEANIcons.swift
-//  GonativeIO
-//
-//  Created by Anuj Sevak on 2021-04-21.
-//  Copyright © 2021 GoNative.io LLC. All rights reserved.
-//
-
+//  LEANIcons.swift - Patched: MedianIcons removed
 import Foundation
-import MedianIcons
+import UIKit
 
-@objc class LEANIcons: NSObject {    
+@objc class LEANIcons: NSObject {
     @objc public static let sharedIcons = LEANIcons()
-    
+
     @objc public class func imageForIconIdentifier(_ name: String, size: CGFloat, color: UIColor) -> UIImage? {
-        return UIImage.init(iconName: name, size: size, color: color)
+        // MedianIcons framework removed - return nil (no custom icons)
+        return nil
     }
 }
