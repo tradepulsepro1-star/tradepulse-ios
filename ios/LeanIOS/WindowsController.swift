ec06f59795bcd5b668ccc55bcffbcc828acec6e8
TradePulse <tradepluspro1@gmail.com>
2026-05-04 17:49:58 +0000

    build 43: remove GoNativeCore imports from all 3 Swift files, stub implementations

--- ios/LeanIOS/WindowsController.swift
+++ ios/LeanIOS/WindowsController.swift
@@ -1,27 +1,13 @@
 //
 //  WindowsController.swift
-//  GonativeIO
-//
-//  Created by Hunaid Hassan on 14.06.21.
-//  Copyright © 2021 GoNative.io LLC. All rights reserved.
+//  TradePulse
 //
 
 import Foundation
-import GoNativeCore
+import UIKit
 
 @objc class WindowsController: NSObject {
     @objc class public func windowCountChanged() {
-        let appConfig = GoNativeAppConfig.shared()!
-        guard LEANWebViewController.currentWindows > appConfig.maxWindows else {
-            return
-        }
-        
-        if let rootViewController = UIApplication.shared.windows.first?.rootViewController as? LEANRootViewController,
-           let navigationController = rootViewController.contentViewController as? UINavigationController {
-            var viewControllers = navigationController.viewControllers
-            let removeTillIndex = LEANWebViewController.currentWindows - appConfig.maxWindows
-            viewControllers.removeSubrange(1...removeTillIndex)
-            navigationController.viewControllers = viewControllers
-        }
+        // no-op stub - GoNativeCore not available
     }
 }
