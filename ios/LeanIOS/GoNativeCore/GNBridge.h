88d95ddc65673390fb744dd43cc413faea87b48a
tradepulsepro1-star <tradepulsepro1@gmail.com>
2026-05-11 16:20:53 -0500

    Add GoNativeCore stub: GNBridge.h

---
+++ ios/LeanIOS/GoNativeCore/GNBridge.h
@@ -0,0 +1,18 @@
+// GNBridge.h — Stub header replacing private GoNativeCore SPM package
+
+#ifndef GNBridge_h
+#define GNBridge_h
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GNBridge : NSObject
+
+- (instancetype)init;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif /* GNBridge_h */
