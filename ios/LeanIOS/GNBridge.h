c7f5e9739fb663e281ccfa16f7c3cd1b461d1957
tradepulsepro1-star <tradepulsepro1@gmail.com>
2026-05-12 04:41:53 -0500

    Fix: GNBridge.h

--- ios/LeanIOS/GNBridge.h
+++ ios/LeanIOS/GNBridge.h
@@ -4,12 +4,23 @@
 #define GNBridge_h
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@protocol GNController;
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface GNBridge : NSObject
 
 - (instancetype)init;
+- (void)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary * _Nullable)options;
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary *)options;
+- (void)applicationDidBecomeActive:(UIApplication *)application;
+- (void)applicationWillResignActive:(UIApplication *)application;
+- (void)applicationDidEnterBackground:(UIApplication *)application;
+- (void)applicationWillEnterForeground:(UIApplication *)application;
+- (nullable id<GNController>)getControllerForKey:(NSString *)key runner:(id _Nullable)runner;
 
 @end
 
