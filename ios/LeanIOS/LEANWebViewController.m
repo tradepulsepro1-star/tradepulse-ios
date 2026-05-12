52fd06bb92639d1be8661c41b603b7e8903fe77e
tradepulsepro1-star <tradepulsepro1@gmail.com>
2026-05-12 02:17:19 -0500

    Fix: replace currentStatusBarFrame with UIWindowScene API for Xcode 26

--- ios/LeanIOS/LEANWebViewController.m
+++ ios/LeanIOS/LEANWebViewController.m
@@ -768,7 +768,13 @@ static NSInteger _currentWindows = 0;
 }
 
 - (void)applyStatusBarOverlay {
-    CGFloat statusBarHeight = [UIApplication sharedApplication].currentStatusBarFrame.size.height;
+    CGFloat statusBarHeight = 0;
+    if (@available(iOS 13.0, *)) {
+        UIWindowScene *scene = (UIWindowScene *)[[[UIApplication sharedApplication] connectedScenes] anyObject];
+        statusBarHeight = scene.statusBarManager.statusBarFrame.size.height;
+    } else {
+        statusBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height;
+    }
     
     // Top guide is equal to super view (below top navbar)
     if ([LEANUtilities isGlassDesignEnabled]) {
@@ -2720,7 +2726,13 @@ static NSInteger _currentWindows = 0;
 {
     if (self.statusBarBackground) {
         // fix sizing (usually because of rotation) when navigation bar is hidden
-        CGSize statusSize = [UIApplication sharedApplication].currentStatusBarFrame.size;
+        CGSize statusSize = CGSizeZero;
+        if (@available(iOS 13.0, *)) {
+            UIWindowScene *scene = (UIWindowScene *)[[[UIApplication sharedApplication] connectedScenes] anyObject];
+            statusSize = scene.statusBarManager.statusBarFrame.size;
+        } else {
+            statusSize = [UIApplication sharedApplication].statusBarFrame.size;
+        }
         CGFloat height = MIN(statusSize.height, statusSize.width);
         // fix for double height status bar on non-iPhoneX
         if (height == 40) {
