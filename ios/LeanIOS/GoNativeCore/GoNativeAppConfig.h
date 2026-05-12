d78db74c206b54ab967dd5b9eff939c6a917a2d7
tradepulsepro1-star <tradepulsepro1@gmail.com>
2026-05-12 03:49:37 -0500

    Fix: sharedAppConfig NS_SWIFT_UNAVAILABLE + instance class property (GoNativeAppConfig.h)

--- ios/LeanIOS/GoNativeCore/GoNativeAppConfig.h
+++ ios/LeanIOS/GoNativeCore/GoNativeAppConfig.h
@@ -13,6 +13,8 @@ NS_ASSUME_NONNULL_BEGIN
 @interface GoNativeAppConfig : NSObject
 
 // Singleton accessors (both ObjC and Swift styles)
+// ObjC singleton accessor (Swift uses .instance class property instead)
++ (instancetype)sharedAppConfig NS_SWIFT_UNAVAILABLE("Use GoNativeAppConfig.instance in Swift");
 @property (class, nonatomic, readonly) GoNativeAppConfig *instance;
 
 // App identity
