561844e56e2d354e75aa8afc367878d4e44acbfb
tradepulsepro1-star <tradepulsepro1@gmail.com>
2026-05-12 03:36:55 -0500

    Fix: use class property .instance instead of sharedXxx factory (GoNativeAppConfig.h)

--- ios/LeanIOS/GoNativeAppConfig.h
+++ ios/LeanIOS/GoNativeAppConfig.h
@@ -13,7 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface GoNativeAppConfig : NSObject
 
 // Singleton accessors (both ObjC and Swift styles)
-+ (instancetype)sharedConfig;
+@property (class, nonatomic, readonly) GoNativeAppConfig *instance;
 
 // App identity
 @property (nonatomic, strong, nullable) NSString *appName;
