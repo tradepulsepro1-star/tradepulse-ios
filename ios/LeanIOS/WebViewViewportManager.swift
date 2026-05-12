68bb9d9f31341a11eb442872d130e6eb3d9309f1
tradepulsepro1-star <tradepulsepro1@gmail.com>
2026-05-12 03:15:38 -0500

    Fix: remove force-unwrap from GoNativeAppConfig.shared() (WebViewViewportManager.swift)

--- ios/LeanIOS/WebViewViewportManager.swift
+++ ios/LeanIOS/WebViewViewportManager.swift
@@ -49,7 +49,7 @@ import WebKit
     }
 
     @objc func setViewport(scale: NSNumber?, width: NSNumber?, webView: WKWebView?) {
-        let appConfig = GoNativeAppConfig.shared()!
+        let appConfig = GoNativeAppConfig.shared()
         
         var scaleContent = ""
         var widthContent = ""
