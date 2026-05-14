88b69ab5de359e30a41b34999097fe421de2ac79
TradePulse <tradepluspro1@gmail.com>
2026-05-14 19:30:10 +0000

    Fix launch crash: revert currentKeyWindow to build 81 implementation (UIApplication.shared.windows)

--- ios/LeanIOS/UIApplication+Extensions.swift
+++ ios/LeanIOS/UIApplication+Extensions.swift
@@ -8,16 +8,13 @@
 
 extension UIApplication {
     @objc public var currentKeyWindow: UIWindow? {
-        return connectedScenes
-            .compactMap { $0 as? UIWindowScene }
-            .flatMap { $0.windows }
-            .last { $0.isKeyWindow }
+        return UIApplication.shared.windows.last { $0.isKeyWindow }
     }
-
+    
     @objc public var currentStatusBarFrame: CGRect {
         return currentKeyWindow?.windowScene?.statusBarManager?.statusBarFrame ?? CGRect()
     }
-
+    
     @objc public var isInterfaceOrientationPortrait: Bool {
         let interfaceOrientation = currentKeyWindow?.windowScene?.interfaceOrientation
         return interfaceOrientation != nil && interfaceOrientation!.isPortrait
